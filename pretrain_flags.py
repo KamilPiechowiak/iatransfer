89dd0224ccb52bc698530b208acf7ebbe04a946a
KamilPiechowiak <kamil.cams@gmail.com>
2020-12-30 23:49:30 +0100

    small models training setup

--- pretrain_flags.py
+++ pretrain_flags.py
@@ -10,29 +10,29 @@ random.seed(RANDOM_STATE)
 torch.manual_seed(RANDOM_STATE)
 np.random.seed(RANDOM_STATE)
 
-FLAGS = {
-    'learning_rate': 1e-2,
-    'epochs': 3,
-    'scheduler_mocked_epochs': 100,
-    'persist_state_every': 2,
-    'repeat': 1,
-    # 'path': '/content/gdrive/My Drive/projects/weightsTransfer/xla_test3',
-    'path': 'res',
-    'bucket_path': 'gs://kamil-piechowiak-weights-transfer/test',
-    'num_workers': 4,
-    'datasets_path': 'data'
-}
-FLAGS['num_cores'] = 8
-
 # FLAGS = {
 #     'learning_rate': 1e-2,
-#     'epochs': 50,
-#     'scheduler_mocked_epochs': 50,
-#     'persist_state_every': 5,
-#     'repeat': 5,
+#     'epochs': 3,
+#     'scheduler_mocked_epochs': 100,
+#     'persist_state_every': 2,
+#     'repeat': 1,
+#     # 'path': '/content/gdrive/My Drive/projects/weightsTransfer/xla_test3',
 #     'path': 'res',
-#     'bucket_path': 'gs://kamil-piechowiak-weights-transfer/stats',
+#     'bucket_path': 'gs://kamil-piechowiak-weights-transfer/test',
 #     'num_workers': 4,
-#     'datasets_path': 'data',
-#     'num_cores': 8
+#     'datasets_path': 'data'
 # }
+# FLAGS['num_cores'] = 8
+
+FLAGS = {
+    'learning_rate': 1e-2,
+    'epochs': 50,
+    'scheduler_mocked_epochs': 50,
+    'persist_state_every': 5,
+    'repeat': 5,
+    'path': 'res',
+    'bucket_path': 'gs://kamil-piechowiak-weights-transfer/stats',
+    'num_workers': 4,
+    'datasets_path': 'data',
+    'num_cores': 8
+}
